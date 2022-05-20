d6254f8d59af6034807de997de1f2e5cb73bc48c
jooyongsim <jysim207@gmail.com>
2022-05-20 17:10:51 +0900

    "save result.csv & clean code"

--- DL_seg_tutorial_2022.py
+++ DL_seg_tutorial_2022.py
@@ -3,14 +3,12 @@ import os
 import time
 
 import torch
-import torch.utils.data
 import torchvision
-import utils
-
 from torch import nn
+import torch.nn.functional as F
 
 import models
-import torch.nn.functional as F
+import utils
 
 import csv
 
@@ -40,7 +38,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
 
         metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
         
-def evaluate(model, data_loader, device, num_classes):
+def evaluate(model, data_loader, device, num_classes, test_only = False):
     model.eval()
     confmat = utils.ConfusionMatrix(num_classes)
     metric_logger = utils.MetricLogger(delimiter="  ")
@@ -50,27 +48,24 @@ def evaluate(model, data_loader, device, num_classes):
             image, target = image.to(device), target.to(device)
             output = model(image)
             output = output["out"]
-
             confmat.update(target.flatten(), output.argmax(1).flatten())
-
         confmat.reduce_from_all_processes()
 
-    return confmat
-            
-def evaluate_submission(model, data_loader, device, num_classes, save_eval_path):
-    model.eval()
-    confmat = utils.ConfusionMatrix(num_classes)
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    header = "Test:"
-    with torch.inference_mode():
-        for image, target in metric_logger.log_every(data_loader, 100, header):
-            image, target = image.to(device), target.to(device)
-            output = model(image)
-            output = output["out"]
-            outputs = output.argmax(1).flatten()
-            for cnt, output in enumerate(outputs):
-                torchvision.utils.save_image(output, os.path.join(save_eval_path, '{cnt}.jpeg')) 
-        confmat.reduce_from_all_processes()
+        if test_only == True:
+            with open('result.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=',')
+                writer.writerow(["ScoreID", "ScoreValue"])
+                _, _, ius = confmat.compute() 
+                ius = ius.cpu().numpy()* 100
+                mius = ius.mean()
+                record_len = len(ius)
+                for cnt, iu in enumerate(ius):
+                    writer.writerow([f"class_{cnt}", ius[cnt]])
+                for i in range(100000):
+                    if i > mius * 1000:
+                        writer.writerow([f"id_{i}", 0])
+                    else:
+                        writer.writerow([f"id_{i}", 1])             
     return confmat
             
 def save_csv(pred_loader, data_loader, device, num_classes):
@@ -108,31 +103,38 @@ def get_transform(train):
     else:
         return utils.SegmentationPresetEval(base_size=520)
 
-
+### (1) Configuration (e.g., folder paths) 
 data_path = '/content/drive/MyDrive/2022_SMWU_Deep_Learning/segmentation' # 'VOC2012'
 model_name =  "deeplabv3_resnet50" # "UNET" # 
 output_dir = 'output_' + model_name # "/content/output" #
 last_ckpt_dir = 'last_ckpt+' + model_name 
-batch_size = 8
-epochs = 50
-lr =0.02
-save_eval_path = 'eval_imgs'
-saved_ckpt_path = '../segmentation/deeplabv3_resnet50_e29.pth'
-prep_submit = True
+saved_ckpt_path = 'output_deeplabv3_resnet50/model_epochXX.pth' # Path to your model 
+
+test_only = False
+resume = False
+start_epoch = 0
+print_freq = 10
+
+device = 'cuda'
+num_classes = 21
 
 if os.path.exists(data_path):
     data_download = False
 else:
     data_download = True
     
-num_classes = 21
+if test_only:
+    resume = True
+
+## (2) define hyperparameters
+batch_size = 8
+epochs = 50
+lr =0.02
+aux_loss = False
 momentum = 0.9
 weight_decay = 1e-4
-print_freq = 10
-aux_loss = False
-start_epoch = 0
-device = 'cuda'
 
+## (3) load dataset
 dataset = torchvision.datasets.VOCSegmentation(data_path, image_set="train", \
     transforms=get_transform(True), download = data_download)
 dataset_test = torchvision.datasets.VOCSegmentation(data_path, image_set="val", \
@@ -154,14 +156,15 @@ data_loader_test = torch.utils.data.DataLoader(
     dataset_test, batch_size=1, sampler=test_sampler, num_workers=16, collate_fn=utils.collate_fn
 )
 
+## (4) define model
 model = models.__dict__[model_name](
     in_channels = 3,
     num_classes=num_classes,
     aux_loss=aux_loss,
     )
-    
 model.to(device)
 
+## (5) define optimizer
 if aux_loss:
     params_to_optimize = [
         {"params": [p for p in model.backbone.parameters() if p.requires_grad]},
@@ -170,7 +173,6 @@ if aux_loss:
     params = [p for p in model.aux_classifier.parameters() if p.requires_grad]
     params_to_optimize.append({"params": params, "lr": lr * 10})
     optimizer = torch.optim.SGD(params_to_optimize, lr=lr, momentum=momentum, weight_decay=weight_decay)
-
 else:
     optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
 
@@ -180,6 +182,18 @@ lr_scheduler = torch.optim.lr_scheduler.LambdaLR(
     optimizer, lambda x: (1 - x / (iters_per_epoch * (epochs))) ** 0.9
 )
 
+# saved_ckpt_path = '....../XXX.pth'
+if resume == True:
+    checkpoint = torch.load(saved_ckpt_path, map_location="cpu")
+    model.load_state_dict(checkpoint["model"], strict=not prep_submit)
+    if not test_only:
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        lr_scheduler.load_state_dict(checkpoint["lr_scheduler"])
+        start_epoch = checkpoint["epoch"] + 1
+
+confmat = evaluate(model, data_loader_test, device=device, \
+    num_classes=num_classes, test_only = test_only)
+print(confmat)
 
 utils.mkdir(output_dir)
 utils.mkdir(last_ckpt_dir)
